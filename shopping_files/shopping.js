6dd8f7db724e4bd921a46662c9d22b78fad985ca
Seth Litthisack <sethlitthisack@protonmail.com>
2020-02-27 15:09:14 -0500

    Added error handling for invalid user input

--- shopping_files/shopping.js
+++ shopping_files/shopping.js
@@ -22,6 +22,7 @@ function promptUser()
 	{
 		// prompt user for required info
 		// if any prompt is left blank, end item prompts
+		// if any input is incorrect, warn user and end item prompts
 		var itemName = prompt("What would you like to buy?");
 		if(!itemName) {break;}
 		var itemPrice = prompt("What is its price?");
@@ -29,13 +30,24 @@ function promptUser()
 		var itemCount = prompt("How many do you want?");
 		if(!itemCount) {break;}
 
-		// append list and increment index i
-		list[i++] =
+		try
 		{
-			name: itemName,
-			price: itemPrice,
-			count: itemCount,
-		};
+			if(typeof itemName != "string") throw "Error! The item name must contain at least one alphanumeric character! Please refresh the page to try again.";
+			if(isNaN(itemPrice)) throw "Error! The item price must be an integer or a decimal! Please refresh the page to try again.";
+			if(itemCount % 1 != 0 || isNaN(itemCount)) throw "Error! The item quantity must be a whole number! Please refresh the page to try again.";
+			list[i++] =
+			{
+				name: itemName,
+				price: itemPrice,
+				count: itemCount,
+			};
+		}
+		catch(error)
+		{
+			alert(error);
+			break;
+		}
+
 
 	} while (true)
 }
